e0fb4e9ee5a728247d5571443b2171a9b9b5dfda
Sean Hurley <sean.hurley6@gmail.com>
2020-07-16 23:02:41 -0600

    Listen for chat messages

--- src/Main.ts
+++ src/Main.ts
@@ -43,32 +43,37 @@ export async function doRoll(event: any, itemName: string): Promise<void> {
   const speaker = ChatMessage.getSpeaker()
   const token = canvas.tokens.get(speaker.token)
   const initiateActor = token ? token.actor : game.actors.get(speaker.actor)
-
-  game.user.targets.forEach(async (target) => {
-    const intendedGM = game.users.entities.find((u: User) => u.isGM && u.active)
-    const attackData = {
-      targetId: target.data._id,
-      totalDamage: 9,
-    }
-    const socketMessage = new Message("action_name", intendedGM!.id, "foobarbazmessage", attackData)
-    manager.broadcastData(socketMessage)
-
-    const actor = target.actor
-    const item = initiateActor.items.find((i: Item) => i.name === itemName)
-    const attributes = actor.data.data.attributes
-
-    await item.rollAttack({ event })
-
-    const templateData = {
-      targetAttributes: attributes,
-      damageAppliedString: "Nailed it.",
-    }
-    sendHitMessage(templateData, actor)
-  })
+  const item = initiateActor.items.find((i: Item) => i.name === itemName)
+  console.log(event)
+  return item.rollAttack({ event })
 }
 
 Hooks.on("hotbarDrop", (_hotbar: any, data: any, slot: number) => {
   log("Handling hotbarDrop")
   createMacro(data.data, slot)
   return false
+})
+
+Hooks.on("renderChatMessage", (message: ChatMessage, _html: any) => {
+  log("Handling renderChatMessage")
+  if (game.user.id === message.user.id && message.isRoll) {
+    game.user.targets.forEach(async (target) => {
+      const intendedGM = game.users.entities.find((u: User) => u.isGM && u.active)
+      const attackData = {
+        targetId: target.data._id,
+        totalDamage: 9,
+      }
+      const socketMessage = new Message("action_name", intendedGM!.id, "foobarbazmessage", attackData)
+      manager.broadcastData(socketMessage)
+
+      const actor = target.actor
+      const attributes = actor.data.data.attributes
+
+      const templateData = {
+        targetAttributes: attributes,
+        damageAppliedString: "Nailed it.",
+      }
+      sendHitMessage(templateData, actor)
+    })
+  }
 })
